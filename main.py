1e95867d0f7b3e6cae7e585a3c8d5ab24c857f04
pwoo9445 <pwoo9445@gmail.com>
2023-03-04 15:57:03 +0900

    3rd commit in notebook

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-## jja csv data conversion code
-
 import pandas as pd
-filedir = 'C:'
-pd.read_excel('')
+filedir = '1sheet.xlsx'
+df = pd.read_excel(filedir, engine='openpyxl', header=3)
+df_date = df.loc[2,:]
+print('df_date',df_date)
+
+df_date = df_date.str.contains('2023')
+
+print('df_date',df_date)
+
+# inst
+df_inst = df.loc[4,:]
+print('df_inst',df_inst)
+# print(df_session)
+
+df_0_index = '날짜'
+df_1_index = '시작'
+df_2_index = '종료'
+df_3_index = '장소'
+df_4_index = '훈련명'
+df_5_index = 'capt'
+df_6_index = 'fo'
+df_7_index = 'inst'
+
+df_new = pd.DataFrame({df_0_index : df_date, df_1_index :df_inst })
+print('df_new.index', df_new.index);exit()
+df_new.to_excel('test2.xlsx')
+# df.to_excel('test.xlsx')
